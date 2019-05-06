14b3f342225a08107ad8c8d0b3e847d5b6eadf41
Dmitry Bogomolov <Dmitry.Bogomolov.mail@gmail.com>
2019-05-06 10:06:40 +0300

    Add more cli (#2)
    * Update dependencies
    
    * Rearrange functions
    
    * Add commander
    
    * Add options
    
    * Implement 'show' options
    
    * Implement other options
    
    * Replace two 'print' options with one
    
    * Rename functions
    
    * Take cwd by default
    
    * Add 'ignore-packages' flag
    
    * Minor change
    
    * Implement 'ignore' options

--- index.js
+++ index.js
@@ -2,8 +2,41 @@
 
 const check = require('monorepo-deps-checker');
 const printList = require('cli-list-select');
+const commander = require('commander');
+const packageInfo = require('./package');
 
-function resolvePackagesVersions(conflicts) {
+function noop() { }
+
+function showPackages(conflicts) {
+    if (!conflicts.length) {
+        console.log('No conflicts');
+        return;
+    }
+    console.log(conflicts.length);
+    conflicts.forEach((conflict) => {
+        console.log(` ${conflict.packageName}:${conflict.section} ${conflict.moduleName} ${conflict.version} -> ${conflict.targetVersion}`)
+    });
+}
+
+function showModules(conflicts) {
+    if (!conflicts.length) {
+        console.log('No conflicts');
+        return;
+    }
+    console.log(conflicts.length);
+    conflicts.forEach((conflict) => {
+        console.log(` ${conflict.moduleName} (${conflict.items.length})`);
+        conflict.items.forEach((item) => {
+            console.log(`   ${item.version} (${item.packages.length})`);
+        });
+    });
+}
+
+function resolveAllPackages(conflicts) {
+    conflicts.forEach(conflict => conflict.resolve());
+}
+
+function resolvePackagesByPrompt(conflicts) {
     if (!conflicts.length) {
         return null;
     }
@@ -22,13 +55,54 @@ function resolvePackagesVersions(conflicts) {
     });
 }
 
-function resolveModulesVersions(conflicts) {
+function resolveModulesByFrequent(conflicts) {
+    conflicts.forEach(conflict => conflict.resolve(0));
+}
+
+function resolveModulesByNew(conflicts) {
+    conflicts.forEach((conflict) => {
+        const list = conflict.items.map(mapItem);
+        list.sort(compareVersions);
+        const choice = list[0].index;
+        conflict.resolve(choice);
+    });
+
+    function mapItem(item, index) {
+        return { index, version: item.version.match(/(\d+)/g) || [] };
+    }
+
+    function compareVersions(version1, version2) {
+        const len = Math.min(version1.length, version2.length);
+        for (let i = 0; i < len; ++i) {
+            const d = version2[i] - version1[i];
+            if (d !== 0) {
+                return d;
+            }
+        }
+        return version2.length - version1.length;
+    }
+}
+
+function resolveModulesByPrompt(conflicts) {
     if (!conflicts.length) {
         return null;
     }
     let currentModuleIndex = -1;
     const moduleVersions = new Map();
-    const printModules = () => {
+    return printModules().then(() => {
+        console.log('VERSIONS');
+        moduleVersions.forEach((versionIndex, moduleIndex) => {
+            const conflict = conflicts[moduleIndex];
+            console.log(` ${conflict.moduleName} ${conflict.items[versionIndex].version}`);
+            conflict.resolve(versionIndex);
+        });
+    });
+
+    function closeList({ end }) {
+        end(true);
+    }
+
+    function printModules() {
         return printList(conflicts, {
             index: currentModuleIndex,
             checks: moduleVersions.keys(),
@@ -40,7 +114,7 @@ function resolveModulesVersions(conflicts) {
                 return `${item.moduleName} (${item.items.length}) ${postfix}`;
             },
             handlers: {
-                'space': ({ end }) => end(1),
+                'space': closeList,
             },
         }).then(({ note, index }) => {
             if (note) {
@@ -48,8 +122,9 @@ function resolveModulesVersions(conflicts) {
                 return printItems();
             }
         });
-    };
-    const printItems = () => {
+    }
+
+    function printItems() {
         const version = moduleVersions.get(currentModuleIndex);
         return printList(conflicts[currentModuleIndex].items, {
             index: version,
@@ -62,7 +137,7 @@ function resolveModulesVersions(conflicts) {
                 return [line, ...lines].join('\n');
             },
             handlers: {
-                'backspace': ({ end }) => end(1),
+                'backspace': closeList,
             },
         }).then(({ note, checks }) => {
             if (!note) {
@@ -74,15 +149,7 @@ function resolveModulesVersions(conflicts) {
             }
             return printModules();
         });
-    };
-    return printModules().then(() => {
-        console.log('VERSIONS');
-        moduleVersions.forEach((versionIndex, moduleIndex) => {
-            const conflict = conflicts[moduleIndex];
-            console.log(` ${conflict.moduleName} ${conflict.items[versionIndex].version}`);
-            conflict.resolve(versionIndex);
-        });
-    });
+    }
 }
 
 // const packagesConflicts = [
@@ -131,7 +198,88 @@ function resolveModulesVersions(conflicts) {
 //     },
 // ];
 
-check(process.argv[2], resolvePackagesVersions, resolveModulesVersions).then(
+function combineFilter(filter, process) {
+    return conflicts => process(filter(conflicts));
+}
+
+function ignorePackages(list, conflicts) {
+    const set = new Set(list);
+    return conflicts.filter(conflict => !set.has(conflict.packageName));
+}
+
+function ignoreModules(list, conflicts) {
+    const set = new Set(list);
+    return conflicts.filter(conflict => !set.has(conflict.moduleName));
+}
+
+function ignoreModulePackages(list, conflicts) {
+    const set = new Set(list);
+    return conflicts.map((conflict) => {
+        const items = conflict.items.map((item) => {
+            const packages = item.packages.filter(obj => !set.has(obj.packageName));
+            return Object.assign({}, item, { packages });
+        });
+        return Object.assign({}, conflict, { items });
+    });
+}
+
+function selectPackagesProcessor(options) {
+    let process;
+    if (options.skipPackages) {
+        process = noop;
+    } else if (options.print) {
+        process = showPackages;
+    } else if (options.resolvePackages) {
+        process = resolveAllPackages;
+    } else {
+        process = resolvePackagesByPrompt;
+    }
+    if (options.ignorePackages) {
+        process = combineFilter(ignorePackages.bind(null, options.ignorePackages), process);
+    }
+    return process;
+}
+
+function selectModulesProcessor(options) {
+    let process;
+    if (options.skipModules) {
+        process = noop;
+    } else if (options.print) {
+        process = showModules;
+    } else if (options.takeNewModule) {
+        process = resolveModulesByNew;
+    } else if (options.takeFrequentModule) {
+        process = resolveModulesByFrequent;
+    } else {
+        process = resolveModulesByPrompt;
+    }
+    if (options.ignoreModules) {
+        process = combineFilter(ignoreModules.bind(null, options.ignoreModules), process);
+    }
+    if (options.ignorePackages) {
+        process = combineFilter(ignoreModulePackages.bind(null, options.ignorePackages), process);
+    }
+    return process;
+}
+
+commander
+    .version(packageInfo.version)
+    .option('--dir [dir]', 'repo directory')
+    .option('--print', 'print conflicts')
+    .option('--skip-packages', 'skip packages conflicts')
+    .option('--skip-modules', 'skip modules conflicts')
+    .option('--resolve-packages', 'resolve all packages conflicts')
+    .option('--take-new-module', 'resolve module conflicts with newest version')
+    .option('--take-frequent-module', 'resolve module conflicts with most frequent version')
+    .option('--ignore-packages [packages]', 'ignored packages', arg => arg.split(','))
+    .option('--ignore-modules [modules]', 'ignored modules', arg => arg.split(','))
+    .parse(process.argv);
+
+const pathToDir = commander.dir || process.cwd();
+const processPackages = selectPackagesProcessor(commander);
+const processModules = selectModulesProcessor(commander);
+
+check(pathToDir, processPackages, processModules).then(
     () => {
         console.log('DONE');
     },
